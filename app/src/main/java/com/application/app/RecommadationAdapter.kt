a8b3c226f297f005e3fb927132e341aac6d8f419
Mufees123 <101590341+Mufees123@users.noreply.github.com>
2022-05-25 16:04:20 +0530

    Third commit

---
+++ app/src/main/java/com/application/app/RecommadationAdapter.kt
@@ -0,0 +1,33 @@
+package com.application.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecommadationAdapter(private val n:List<RecommadationModal>):RecyclerView.Adapter<RecommadationAdapter.ViewHolder>() {
+
+    class ViewHolder(Recommadation: View):RecyclerView.ViewHolder(Recommadation){
+        val image:ImageView=Recommadation.findViewById(R.id.imageUpsc11);
+        val text:TextView=Recommadation.findViewById(R.id.txtApplyNow);
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.recommendations,parent,false);
+        return  ViewHolder(view);
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+        val item=n[position];
+        holder.image.setImageResource(item.image);
+        holder.text.text=item.name
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/application/app/RecommadationModal.kt
@@ -0,0 +1,5 @@
+package com.application.app
+
+class RecommadationModal(val image:Int,val name:String) {
+
+}
