9593d50395fa7b5f8066df8a95ea9049cf037d4e
Mufees123 <101590341+Mufees123@users.noreply.github.com>
2022-05-26 21:00:00 +0530

    update2

---
+++ app/src/main/java/com/application/app/ExamPattern.kt
@@ -0,0 +1,11 @@
+package com.application.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExamPattern : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exam_pattern)
+    }
+}
