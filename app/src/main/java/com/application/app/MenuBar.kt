615e3789b33ebc02fd213bb9d5338f9f24b40e1e
Mufees123 <101590341+Mufees123@users.noreply.github.com>
2022-05-29 11:15:45 +0530

    update2

---
+++ app/src/main/java/com/application/app/MenuBar.kt
@@ -0,0 +1,13 @@
+package com.application.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_bar)
+
+
+    }
+}
